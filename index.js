953fe99b76c41279645f16f34b0461e86060756e
JulianaAmoasei <late.parrot@gmail.com>
2022-07-20 16:48:15 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const banana = "banana";
+const numero = 55;
+
+console.log(banana, numero);
+
+
+
+
+// primeiros passos
+
+// iniciar um repositório git com o comando `git init`
+
+  // cria uma pasta oculta `.git` no seu LOCAL (no computador)
+  // Initialized empty Git repository in /home/juliana/Documents/laboratoria/SAP008/oficina-git-sap008/.git/
+
+// criar no github.com o repositório (https://github.com/seunome/nomedoprojeto.git)
+  // nesse momento o local e o remoto AINDA NÃO ESTÃO CONECTADOS
+
+// linkar o local com o remoto 
+  //git remote add origin git@github.com:JulianaAmoasei/oficina-git-sap008.git
+
+// sincronizar o local com o remoto
+  // TRÊS COMANDOS PRINCIPAIS DO GIT (por enquanto)
+  // `git add .`
